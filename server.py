cf664a031918c9fb92a40302999dc4ff989c836e
mavas <10604882+mavas@users.noreply.github.com>
2020-04-13 03:38:32 -0700

    Got the cipher method implemented

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
+import string
+
 from flask import Flask, request
 
 
 app = Flask(__name__)
 
 
+def caesar(plaintext, shift):
+    """https://stackoverflow.com/questions/8886947/caesar-cipher-function-in-python"""
+    alphabet = string.ascii_lowercase
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return plaintext.translate(table)
+
+
 @app.route('/api/encode/', methods=['POST'])
 def hello_world():
-    data = request.data
-    print(data)
-    print(request.args)
-    print(request.json)
-    #print(dir(request))
+    if request.is_json:
+        data = request.json
+        if 'Shift' in data and 'Message' in data:
+            return caesar(data['Message'], data['Shift'])
     #rval = {'EncodedMessage': rval}
     return 'Hello, World!'
 
